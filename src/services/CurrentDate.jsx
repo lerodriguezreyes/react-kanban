aa880035eff84e5e270148873f50cca33fcc48ab
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-02-29 10:30:57 -0400

    deleted files.

--- src/services/CurrentDate.jsx
+++
@@ -1,9 +0,0 @@
- export function CurrentDate() {
-    const date = new Date();
-    const dd = date.getDate();
-    const mm = date.getMonth()+1;
-    const mmEdit = mm.toString().padStart(2, '0');
-    const yyyy = date.getFullYear();
-    const currentDate = `${yyyy}-${mmEdit}-${dd}`;
-    return currentDate;
-    }
