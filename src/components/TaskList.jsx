4bff8c5bc79144bcd4b7872ca2511c0a78d56f6c
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-02-29 20:16:14 -0400

    refactored code into a cleaner version.

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,48 +1,26 @@
 import KanbanTaskData from "../assets/KanbanData.json";
 import { useState } from "react";
-import { currentDate, milisecondComparison } from "../services/DateUtilities";
+import TaskCard from "./TaskCard";
 
 function TaskList() {
-  const [task, setTask] = useState(KanbanTaskData);
+  const [tasks, setTasks] = useState(KanbanTaskData);
 
-  const deleteTaskElement = (taskElementId) => {
-    const filteredTaskArray = task.filter((taskElement) => {
-      return taskElement.id !== taskElementId;
+  const deleteTask = (taskId) => {
+    const filteredTasks = tasks.filter((task) => {
+      return task.id !== taskId;
     });
 
-    setTask(filteredTaskArray);
+    setTasks(filteredTasks);
   };
 
-  return task.map((taskElement) => {
-    return (
-      <div key={taskElement.id} className="TaskCard">
-        <h3>Task: {taskElement.title}</h3>
-        <p> task description: {taskElement.description}</p>
-        <p> assignee: {taskElement.assignee}</p>
-        <p> priority: {taskElement.priority}</p>
-        <p> createdDate: {taskElement.createdDate}</p>
-        <p> dueDate: {taskElement.dueDate}</p>
-        {milisecondComparison(taskElement.dueDate) ? (
-          <p>
-            {" "}
-            On Time<span>✔️</span>{" "}
-          </p>
-        ) : (
-          <p>
-            {" "}
-            <span>⏰</span>Prioritize this task!<span>⏰</span>
-          </p>
-        )}
-
-        <button
-          onClick={() => deleteTaskElement(taskElement.id)}
-          className="deletebtn"
-        >
-          Delete task{" "}
-        </button>
-      </div>
-    );
-  });
+  return (
+    <div>
+      <h2> Task List </h2>
+      {tasks.map((task) => {
+        return <TaskCard key={task.id} task={task} deleteTask = {deleteTask}/>;
+      })}
+    </div>
+  );
 }
 
 export default TaskList;
