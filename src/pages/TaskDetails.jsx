22b73fa108f13f3a9856fe7ccce73dd10e63acea
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-02-29 23:19:33 -0400

    started work on task details

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -1,8 +1,36 @@
+import { Link } from "react-router-dom"
 
-function TaskDetails() {
+function TaskDetails(task) {
   return (
-    <div>TaskDetails</div>
+    <div>
+    <h2>Celestial body details</h2>
+    <TaskCard/>
+    </div>
   )
 }
 
 export default TaskDetails
+
+
+
+
+
+// // src/pages/ProjectsPage.jsx
+
+// function ProjectsPage(props) {
+//   return (
+//     <div>
+//       <h1>Projects</h1>
+//       {props.projects.map((project) => {
+//         return (
+//           <div key={project._id} className="project">
+//             <h3>{project.name}</h3>
+//             <p>{project.technologies}</p>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default ProjectsPage;
