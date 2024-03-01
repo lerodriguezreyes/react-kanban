f8c76c7cf0bb90a151680cb46177f9a363c7330b
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-02-29 23:18:47 -0400

    created images object.

---
+++ src/images/images.jsx
@@ -0,0 +1,7 @@
+const Images = {
+        image1 : new URL('./flownaut.png', import.meta.url).href,
+        image2 : new URL('./lost.png', import.meta.url).href
+    }
+    
+
+export default Images
