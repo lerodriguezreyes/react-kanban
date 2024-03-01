9877e6269a283864a30514a1216bd77d42c6a0aa
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-02-29 20:16:43 -0400

    Generated new pages templates for routing.

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+
+function NotFound() {
+  return (
+    <div>Error</div>
+  )
+}
+
+export default NotFound
---
+++ src/pages/TaskDetails.jsx
@@ -0,0 +1,8 @@
+
+function TaskDetails() {
+  return (
+    <div>TaskDetails</div>
+  )
+}
+
+export default TaskDetails
