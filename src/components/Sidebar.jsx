1e3845214764dec5d95a5f8d6540f7a899b6bb22
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-02-29 22:24:32 -0400

    set up links in better format

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom"
+
 function Sidebar() {
   return (
     <>
     <hr></hr>
     <div>Sidebar</div>
     <ul>
-    <li> Home </li>
-    <li> About </li>
-    <li> Space Images Gallery </li>
+      <li><Link to="/"> Home </Link> </li>
+      <li><Link to="/missionboard"> Mission Board </Link> </li>
+      <li><Link to="/about"> About </Link></li>
+      <li><Link to="/gallery"> Space Images Gallery </Link></li>
     </ul>
     </>
   )
