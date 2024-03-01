abfc08d9cf087c8149a9234c3b901874ea397dc0
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-02-29 23:18:23 -0400

    removed import line not in use.

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,6 +1,5 @@
 import KanbanTaskData from "../assets/KanbanData.json";
 import { useState } from "react";
-import TaskCard from "./TaskCard";
 import ReducedTaskCard from "./ReducedTaskCard";
 
 function TaskList() {
