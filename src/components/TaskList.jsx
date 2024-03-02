7c38d18393f53bbfbf152b1cb0dd6392cbbbb53c
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-02 12:09:27 -0400

    Linked add new task to list of task items.

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,10 +1,14 @@
 import KanbanTaskData from "../assets/KanbanData.json";
 import { useState } from "react";
 import ReducedTaskCard from "./ReducedTaskCard";
+import AddTask from "./AddTask";
 
 function TaskList() {
   const [tasks, setTasks] = useState(KanbanTaskData);
-
+  const addNewTask = (newTask) => {
+    let newTasks = [newTask, ...tasks]
+    setTasks(newTasks)
+  } 
   const deleteTask = (taskId) => {
     const filteredTasks = tasks.filter((task) => {
       return task.id !== taskId;
@@ -16,6 +20,7 @@ function TaskList() {
   return (
     <div>
       <h2> Orbiting tasks </h2>
+      <AddTask addNewTask = {addNewTask} />
       {tasks.map((task) => {
         return <ReducedTaskCard key={task.id} task={task} deleteTask = {deleteTask}/>;
       })}
