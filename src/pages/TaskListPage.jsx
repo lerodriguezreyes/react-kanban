107b386519a1cb7515873650d836334a59c0eebb
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-04 23:16:02 -0400

    started creating tasklistpage

---
+++ src/pages/TaskListPage.jsx
@@ -0,0 +1,12 @@
+import TaskList from "../components/TaskList"
+
+function TaskListPage() {
+  return (
+    <div>
+    <h1> Pending Tasks list </h1>
+    <TaskList />
+    </div>
+  )
+}
+
+export default TaskListPage
